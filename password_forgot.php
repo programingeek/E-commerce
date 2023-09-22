ff9d637a7724df837c24677a93714f71fe0e6917
@WHITE_DEVIL_69 <103433497+programingeek@users.noreply.github.com>
2023-09-22 14:15:36 +0530

    Add files via upload

---
+++ password_forgot.php
@@ -0,0 +1,64 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition login-page">
+<div class="login-box">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="styleSheet" href="index.css">
+    <title>K.zone</title>
+</head>
+<body style="background-color:rgb(217, 217, 217) ;">
+    <header data-color="default" class="header header--visible">
+        <img class="img-responsive" src="Ecommerce Site PHP/ecommerce/index.php" alt="Shop Logo">
+
+        <h1>
+            K.Zone Shopping
+        </h1>
+    </header>
+    
+  	<?php
+      if(isset($_SESSION['error'])){
+        echo "
+          <div class='callout callout-danger text-center'>
+            <p>".$_SESSION['error']."</p> 
+          </div>
+        ";
+        unset($_SESSION['error']);
+      }
+      if(isset($_SESSION['success'])){
+        echo "
+          <div class='callout callout-success text-center'>
+            <p>".$_SESSION['success']."</p> 
+          </div>
+        ";
+        unset($_SESSION['success']);
+      }
+    ?>
+  	<div class="login-box-body">
+    	<p class="login-box-msg">Enter email associated with account</p>
+
+    	<form action="reset.php" method="POST">
+      		<div class="form-group has-feedback">
+        		<input type="email" class="form-control" name="email" placeholder="Email" required>
+        		<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      		</div>
+      		<div class="row">
+    			<div class="col-xs-4">
+          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="reset"><i class="fa fa-mail-forward"></i> Send</button>
+        		</div>
+      		</div>
+    	</form>
+      <br>
+      <a href="login.php">I rememberd my password</a><br>
+      <a href="index.php"><i class="fa fa-home"></i> Home</a>
+  	</div>
+</div>
+	
+<?php include 'includes/scripts.php' ?>
+</body>
+</html>
